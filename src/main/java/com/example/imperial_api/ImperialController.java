a4bd27f02cb29c6650df9dd3f7af4acdb5b5cddb
Maria Puthenveetil <wi21b092@technikum-wien.at>
2022-06-22 16:20:04 +0200

    added ounceToGram function

---
+++ src/main/java/com/example/imperial_api/ImperialController.java
@@ -0,0 +1,23 @@
+package com.example.imperial_api;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class ImperialController {
+
+    private Imperial imperial = new Imperial();
+
+    @RequestMapping("/ounceToGram")
+    public double ounceToGram(
+            @RequestParam double a
+    ){
+        return imperial.ounceToGram(a);
+    }
+
+
+}
