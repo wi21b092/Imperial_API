70b616f88bf901946f721778790f797feb1961a7
Maria Puthenveetil <wi21b092@technikum-wien.at>
2022-06-22 15:27:09 +0200

    Initial commit

---
+++ src/main/java/com/example/imperial_api/ImperialApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.imperial_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImperialApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImperialApiApplication.class, args);
+    }
+
+}
